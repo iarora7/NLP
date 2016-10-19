61d6f647826df6ddf28360403a27c390f5a4c7bf
Isha Arora <iarora@usc.edu>
2016-10-19 00:32:19 -0700

    avg_per_model

--- assignment2/avg_per_learn.py
+++ assignment2/avg_per_learn.py
@@ -2,22 +2,26 @@ import os
 import json
 import random
 import sys
-from collections import defaultdict
 import time
+from collections import defaultdict
 
 # path = r"/Users/arorai/USC/sem3/nlp/assignments/NLP/assignment2/files/train"
+# path = r"/Users/arorai/USC/sem3/nlp/assignments/NLP/assignment2/files1"
 # path = r"/Users/arorai/USC/sem3/nlp/assignments/NLP/assignment2/Sample/train"
 path = sys.argv[1]
 # path=r"/Users/isha/USC/sem3/NLP/assignments/NLP/assignment2/Sample/train"
 # path=r"/Users/isha/USC/sem3/NLP/assignments/NLP/assignment2/files/train"
 
 start_time = time.time()
-all_files=dict()
 wd=defaultdict(int)
+b=0
+ud=defaultdict(int)
+beta=0
+c=1
 file_count=0
 file_list=[]
-b=0
 maxitr = 2
+all_files=dict()
 
 def readFile(fpath):
     myfile = open(fpath, 'r', encoding="latin1")
@@ -29,6 +33,7 @@ def readFile(fpath):
             continue
         else:
             wd[word] = 0
+            ud[word] = 0
 
 for dirName, subdirList, fileList in os.walk(path):
     for fname in fileList:
@@ -50,6 +55,7 @@ for i in range(0, maxitr):
             y = 1
         else:
             y = -1
+        # calculate xd (vector of input data features)
         for word in filedata:
             if word in xd:
                 xd[word] = xd[word] + 1
@@ -62,11 +68,19 @@ for i in range(0, maxitr):
         if (y * a) <= 0:
             for word in filedata:
                 wd[word] = wd[word] + (y * xd[word])
+                ud[word] = ud[word] + (y * c * xd[word])
             b = b + y
+            beta = beta + (y*c)
+        c = c + 1
+
+for k,v in ud.items():
+    ud[k] = wd[k] - ((1/c) * ud[k])
+
+beta = b - ((1/c) * beta)
 
 data = {
-    "b": b,
-    "wd": wd
+    "b": beta,
+    "wd": ud
 }
 json_data = json.dumps(data)
 model = open('per_model.txt', 'w')
@@ -75,8 +89,4 @@ model.close()
 
 print("filecount:", file_count)
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
-
 
--- assignment2/per_classify.py
+++ assignment2/per_classify.py
@@ -1,20 +1,26 @@
 import json;
 import sys
 import os
+import time
 
-# path = sys.argv[1]
-# opfile = sys.argv[2]
-opfile = 'per_output.txt'
-path=r"/Users/isha/USC/sem3/NLP/assignments/NLP/assignment2/Sample/test"
+path = sys.argv[1]
+opfile = sys.argv[2]
+# opfile = 'per_output.txt'
+# path=r"/Users/isha/USC/sem3/NLP/assignments/NLP/assignment2/Sample/test"
+# path=r"/Users/isha/USC/sem3/NLP/assignments/NLP/assignment2/files/dev"
 
+start_time = time.time()
 wd=dict()
 b = 0
 op = 0
+all_files=dict()
+file_count=0
 
 output_file = open(opfile, 'w')
 
-model_file = open("per_model.txt", 'r', encoding='latin1')
+model_file = open("per_model.txt", 'r', encoding="latin1")
 file_data = model_file.read()
+model_file.close()
 file_data = json.loads(file_data)
 for k in file_data:
     if (k == "b"):
@@ -25,26 +31,25 @@ for k in file_data:
 for dirName, subdirList, fileList in os.walk(path):
     for fname in fileList:
         fpath = os.path.join(dirName, fname)
-        if "DS_Store" in fpath or "__MACOSX" in fpath:
-            break
-        else:
-            a=0
-            xd = dict()
-            myfile = open(fpath, 'r', encoding="latin1")
-            filedata = myfile.read().split()
-            for word in filedata:
-                if word in xd:
-                    xd[word] = xd[word]+1
-                else:
-                    xd[word] = 1
-            for word in filedata:
-                if (word in wd and word in xd):
-                    a += (wd[word] * xd[word])
-            a = a + b
-            if(a>0):
-                op = "SPAM"
+        a=0
+        xd = dict()
+        myfile = open(fpath, 'r', encoding="latin1")
+        filedata = myfile.read().split()
+        for word in filedata:
+            if word in xd:
+                xd[word] = xd[word]+1
             else:
-                op = "HAM"
-            output_file.write(op + " " + fpath + "\n")
+                xd[word] = 1
+        for word in filedata:
+            if (word in wd and word in xd):
+                a += (wd[word] * xd[word])
+        a = a + b
+        if(a>0):
+            op = "SPAM"
+        else:
+            op = "HAM"
+        output_file.write(op + " " + fpath + "\n")
 
 output_file.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
